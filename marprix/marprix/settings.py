d9707f52f7c7fff9cda47fa17fb7982d5081d83a
Allard <allard.oelen@gmail.com>
2018-05-01 20:26:11 +0200

    deploy

--- marprix/marprix/settings.py
+++ marprix/marprix/settings.py
@@ -94,8 +94,9 @@ DATABASES = {
     }
 }'''
 
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
-
+DATABASES = {
+    "default": dj_database_url.config()
+}
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
