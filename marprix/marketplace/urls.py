45e022b60578d0be1086f72995e456cbe1406c50
Allard <allard.oelen@gmail.com>
2018-05-01 13:26:44 +0200

    First start of Voice XML

--- marprix/marketplace/urls.py
+++ marprix/marketplace/urls.py
@@ -6,5 +6,7 @@ app_name = 'marketplace'
 urlpatterns = [
     path('', views.index, name='index'),
     path('market-leader', views.housekeeping, name='housekeeping'),
-    path('save', views.save, name='save')
+    path('save', views.save, name='save'),
+    path('VXML/welcome', views.voice_welcome, name='voice_welcome'),
+    path('VXML/categories', views.voice_categories, name='voice_categories')
 ]
--- marprix/marketplace/views.py
+++ marprix/marketplace/views.py
@@ -30,3 +30,17 @@ def housekeeping(request):
 
 def save(request):
     a=1+1
+
+# Voice application
+
+def voice_welcome(request):
+    return render(request, 'marketplace/voice_xml/welcome.xml', content_type="application/xhtml+xml")
+
+def voice_categories(request):
+    all_categories = Category.objects.all().order_by('sort');
+
+    context = {
+        'all_categories': all_categories,
+    }
+
+    return render(request, 'marketplace/voice_xml/categories.xml', context, content_type="application/xhtml+xml")
