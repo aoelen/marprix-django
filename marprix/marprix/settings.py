e83d68fd18f94f64a9e1739eb3af609f52ea5054
Allard <allard.oelen@gmail.com>
2018-05-01 16:49:35 +0200

    Publish on Heroku

--- marprix/marprix/settings.py
+++ marprix/marprix/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&s#hd3&pg4i#*g*59m)nh_2!%^iw0_xqt(z+fyxju99j3b99hf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
