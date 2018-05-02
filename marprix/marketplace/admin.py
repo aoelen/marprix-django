17195ecfba7bf0fb0997ee4aa66a7b7c200783ac
Allard <allard.oelen@gmail.com>
2018-05-02 22:32:39 +0200

    History admin add

--- marprix/marketplace/admin.py
+++ marprix/marketplace/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Product, Category, Unit
+from .models import Product, Category, Unit, History
 
 # Make the models accessible via the Django admin panel
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Unit)
+admin.site.register(History)
