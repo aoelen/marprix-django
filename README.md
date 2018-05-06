06618e258bd06de1ae71207304717b70e277e68c
Allard <allard.oelen@gmail.com>
2018-05-06 23:07:10 +0200

    Improve README + add comments to code

--- README.md
+++ README.md
@@ -8,18 +8,18 @@
 > - Tom Tol
 > - Allard Oelen
 
-## Introduction
+## 1. Introduction
 Marprice provides farmers in rural areas of Africa the most recent market prices. Marprice offers both the front-end and back-end. In the back-end products can be added, delete or product prices can be updated. The front-end can be accessed from both a web interface and via a voice interface. The full version working version of this application can be accessed via: http://marprice.herokuapp.com/
 
 _The names Marprice and Marprix are used interchangeably. Both the names refer to the same application._
 
-## Installation requirements
+## 2. Installation requirements
 - Python 3.6 or higher
 - Django 2
 - Postgresql
 - No internet connection required
 
-## Installation manual
+## 3. Installation instructions
 Local installation (make sure to have Python and Django installed first, optionally work in a virtual environment)
 1. `$ git clone https://github.com/aoelen/marprix-django`
 2. Install the required packages: `$ pip install -r requirements.txt`
@@ -28,8 +28,26 @@ Local installation (make sure to have Python and Django installed first, optiona
 4. Run the server: `$ python marprix/manage.py runserver`
 5. Visit Marprice at: `http://localhost:8000`
 
-# Prices web interface
+## 4. Application functionality
+In this section, the functionality of Marprice is briefly explained. For a detailed description of this application, please have a look at our project report
 
-# Prices voice interface
+### 4.1 Prices web interface
+Link: http://marprice.herokuapp.com/ This web interfaced can be accessed by farmers, to check out the latest market prices. The categories, products and prices are loaded from the database. Each product is related to a unit, since each product has its own measurement unit.
 
-# Market leader housekeeping
+### 4.2 Prices voice interface
+Link: http://marprice.herokuapp.com/VXML/welcome Since the application is mainly designed for use in rural Africa, the voice interface is the most prominent part of Marprice. The voice interface provides an intuitive way of requesting product pricing using DTMF menus. Since Text-To-Speech (TTS) is not always available/reliable, all the audio files are stored locally. This also ensures that Marprice can work without an internet connection. In all voice menus, their is an option to go back to the previous action in order to ensure that user errors can be recovered.
+
+#### 4.2.1 Welcome & History
+Farmers are welcomed when they call Marprice for the first time. If they requested a product price before, they can request the last requested price (a full product history per phone number is stored in the database)
+
+#### 4.2.2 Request product per ID
+If a farmer is already familiar with the system, they can enter their product ID directly in Marprice. This saves time, since they can skip the category and product selection.
+
+#### 4.2.3 Product categories & product selection
+In the product categories menu, farmers can select a category from the product they are looking for. The categories are dynamically loaded from the database. After a category has been selected, products can be selected. After a product is selected, the price and unit are provided. In addition to this, the selection is saved to the database for both statistics and the history functionality.
+
+### 4.3 Market leader housekeeping
+Link: http://marprice.herokuapp.com/market-leader Via this interface, a market leader (or NGO) can enter the latest prices, add products or delete products.
+
+## 5. Application code
+Since this application is built using the Django web framework, some code in this repository is needed for Django and is not very interesting to look into. The most important file is: `marprix/marketplace/views.py`. In this file the views are defined, also the functions in this file are commented.
--- marprix/marketplace/views.py
+++ marprix/marketplace/views.py
@@ -1,27 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
-
+# Import the needed models
 from .models import Product, Category, Unit, History
-# Create your views here.
+
 
 def index(request):
-    #return HttpResponse('HELLO TO MARKET LEADER')
-    all_categories = Category.objects.all().order_by('sort');
-    all_products = Product.objects.select_related('unit').all().order_by('name');
+    '''
+    Function for the front-end to request product prices
+    '''
 
+    all_categories = Category.objects.all().order_by('sort'); # Get all categories, order by a predefined order in the database
+    all_products = Product.objects.select_related('unit').all().order_by('name'); # Get all products, and related units, ordered by name
+
+    # Pass parameters to the template
     context = {
         'all_categories': all_categories,
         'all_products' : all_products
     }
 
+    # Render the .html file
     return render(request, 'marketplace/index.html', context)
 
 def housekeeping(request):
-
+    '''
+    Shows the housekeeping page where the market leader can enter the prices
+    '''
+    # Get all needed data
     all_categories = Category.objects.all().order_by('sort')
     all_products = Product.objects.all().order_by('name')
     all_units = Unit.objects.all().order_by('name')
 
+    # Get the query string to show a flash message after saving
     success = request.GET.get('success', False)
 
     context = {
@@ -34,28 +43,45 @@ def housekeeping(request):
     return render(request, 'marketplace/housekeeping.html', context)
 
 def save(request):
+    '''
+    Save the data posted in the houseeking file
+    '''
+    # Get all the POST parameters, which are arrays
     names = request.POST.getlist('name[]')
     prices = request.POST.getlist('price[]')
     units = request.POST.getlist('unit[]')
     categories = request.POST.getlist('category[]')
 
+    # Delete all products
     Product.objects.all().delete()
 
+    # Enter all products again in the database
     for index, name in enumerate(names):
         new_product = Product(name=name,category_id=categories[index],unit_id=units[index],price=prices[index])
         new_product.save()
 
+    # Redirect, show success message
     return HttpResponseRedirect('/market-leader?success=True')
 
 def save_history(request, product_id):
+    '''
+    This functions saves the phone number and product requested (for the history function)
+    '''
+    # Phone numbers
     callerid = request.session.get('callerid', 0)
+    # Save history
     new_history = History(callerid=callerid,last_product_id=product_id)
     new_history.save()
 
 # Voice application
 
 def voice_welcome(request):
+    '''
+    Welcome the caller function, renders are VoiceXML
+    '''
+    # Get the phone numbers, if not provided: unknown
     callerid = request.GET.get('callerid', 'Unknown')
+    # Get the history of the caller, to find out this it is a returning client
     caller_has_history = History.objects.all().filter(callerid=callerid)
 
     # Is the caller an returning client?
@@ -64,15 +90,20 @@ def voice_welcome(request):
     else:
         returning_client = True
 
+    # Place the phone number in a session to later save the history
     request.session['callerid'] = callerid
 
     context = {
         'returning_client': returning_client
     }
 
+    # Render the voice XML
     return render(request, 'marketplace/voice_xml/welcome.xml', context, content_type="application/xhtml+xml")
 
 def voice_categories(request):
+    '''
+    Get all the product categories
+    '''
     all_categories = Category.objects.all().order_by('sort');
 
     context = {
@@ -82,6 +113,9 @@ def voice_categories(request):
     return render(request, 'marketplace/voice_xml/categories.xml', context, content_type="application/xhtml+xml")
 
 def ids(request):
+    '''
+    File for entering a product ID directly
+    '''
     all_products = Product.objects.all().order_by('id');
 
     context = {
@@ -91,11 +125,12 @@ def ids(request):
     return render(request, 'marketplace/voice_xml/ids.xml', context, content_type="application/xhtml+xml")
 
 def products(request, category_id):
-    selected_products = Product.objects.all().filter(category_id=category_id).order_by('name');
-    '''callerid = request.session.get('callerid', 0)
-    new_history = History(callerid=callerid,last_product_id=163)
-    new_history.save()
     '''
+    Lists all the products
+    '''
+
+    selected_products = Product.objects.all().filter(category_id=category_id).order_by('name');
+
     context = {
         'selected_products': selected_products,
         'category_id': category_id
@@ -104,6 +139,10 @@ def products(request, category_id):
     return render(request, 'marketplace/voice_xml/products.xml', context, content_type="application/xhtml+xml")
 
 def select_product(request, product_id):
+    '''
+    Select a product
+    This file is seperated from the products file, to make it possible to save the history
+    '''
     product = Product.objects.get(id=product_id);
 
     save_history(request, product_id)
@@ -115,6 +154,9 @@ def select_product(request, product_id):
     return render(request, 'marketplace/voice_xml/select_product.xml', context, content_type="application/xhtml+xml")
 
 def select_id(request, product_id):
+    '''
+    Product selected based on ID
+    '''
     product = Product.objects.get(id=product_id);
 
     save_history(request, product_id)
@@ -127,6 +169,9 @@ def select_id(request, product_id):
 
 
 def history(request):
+    '''
+    Get the latest product ID requested by this caller
+    '''
     callerid = request.session.get('callerid', 0)
     history = History.objects.all().filter(callerid=callerid).order_by('-id')
     history_product_id = history[0].last_product_id
@@ -137,17 +182,3 @@ def history(request):
     }
 
     return render(request, 'marketplace/voice_xml/history.xml', context, content_type="application/xhtml+xml")
-
-
-
-'''
-def test_history(request):
-    new_history = History(callerid='00',last_product_id=163)
-    new_history.save()
-
-    context = {
-        'returning_client': False
-    }
-
-    return render(request, 'marketplace/voice_xml/welcome.xml', context, content_type="application/xhtml+xml")
-'''
